92275075c3d507f9fef0c82441e3f761c265cee1
ShanchaiZ <shanchai.zahid@gmail.com>
2022-08-18 19:50:20 -0400

    added basic app logic in a while loop with quit feature

---
+++ script.js
@@ -0,0 +1,8 @@
+//User Input and Quit:
+let input = prompt("what would you like to do?");
+
+while (input !== "quit") {
+    input = prompt("what would you like to do?");
+};
+
+console.log("User Has Quit the App!");
