64f3e6f15559094134315246bb2f7a0a32a42ef6
ShanchaiZ <shanchai.zahid@gmail.com>
2022-08-22 15:01:03 -0400

    created a storage array for todo list and implemented list function to list the index and array item using a for loop

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-//User Input and Quit:
+//1. USER INPUT AND QUIT FUNCTION:
 let input = prompt("what would you like to do?");
 
+//2. CREATE AN ARRAY FOR STORAGE:
+const todos = ["Go Grocery Shopping", "Fix the Car", "Buy pens", "Watch a Movie"];
+
 while (input !== "quit" && input !== "q") {
+
+    //3. MAKE A LIST INSIDE THE ARRAY
+    if (input === "list") {
+        console.log("************************");
+        //list the arrays and its index using a for loop:
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log("************************");
+    }
+
     input = prompt("what would you like to do?");
 };
 
